07e7279205b6f04ad5a4717aa21c336cdaca596d
mrinal-at-git <60399993+mrinal-at-git@users.noreply.github.com>
2020-07-06 22:40:25 +0530

    Create determineCycle.cpp

---
+++ Linked List/determineCycle.cpp
@@ -0,0 +1,22 @@
+Question Link - https://leetcode.com/problems/linked-list-cycle/
+
+  bool hasCycle(ListNode *head) {
+        
+  
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        
+        while(fast!=NULL && fast->next!=NULL){ 
+            
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if(slow == fast) return true;
+            
+        }
+    
+      return false; 
+   
+    }
